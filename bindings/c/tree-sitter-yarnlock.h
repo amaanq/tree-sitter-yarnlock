a11d09bd7e3702116dd4cf85901a1897aefa5d2d
Amaan Qureshi <amaan.qureshi@datadoghq.com>
2024-07-18 16:48:20 -0400

    initial commit

---
+++ bindings/c/tree-sitter-yarnlock.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_YARNLOCK_H_
+#define TREE_SITTER_YARNLOCK_H_
+
+typedef struct TSLanguage TSLanguage;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const TSLanguage *tree_sitter_yarnlock(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TREE_SITTER_YARNLOCK_H_
